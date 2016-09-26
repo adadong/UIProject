1015f74bc887f253e6545babe7ac5b9f7037262f
Ada Dong <fi_dongxueling@163.com>
2016-09-26 10:49:33 +0800

    ada push

---
+++ app/src/main/java/com/ada/testapplication/AutoCompleteTextViewActivity.java
@@ -0,0 +1,13 @@
+package com.ada.testapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AutoCompleteTextViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auto_complete_text_view);
+    }
+}
